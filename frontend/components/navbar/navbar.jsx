b9a537eea3f085e95272eba867f5a26c11388df2
Domenico Daniel Giovinazzo <domenicodanielgiovinazzo@gmail.com>
2021-03-03 22:02:58 -0500

    Beer show page

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,37 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom'
 
-class NavBar extends React.Component {
+const NavBar = ({ currentUserId, logout }) => {
 
-    render() {
-        return (
-            <div className='header-container'>
-                <div className='header-content-container'>
+    const [renderDropdown, setRenderDropdown] = useState(false)
+    const dropdown = (
+        <div className="dropdown-outer" >
+
+            <div  className="dropdown">
+                <i className="arrow"></i>
+                <Link className='nav-links' to={`/user/${currentUserId}`}>My Profile</Link>
+                <p onClick={() => logout()} className='nav-links' >Logout</p>
+            </div>
+        </div>
+    )
+
+    return (
+        <div className='header-container'>
+            <div className='header-content-container'>
 
 
-                        <Link to='/home' ><img  src={window.homeLogo} alt="" /></Link>
+                <Link to='/home' ><img src={window.homeLogo} alt="" /></Link>
 
                 <div className="nav-content-right">
 
-                        <div className="dropdown-container">
-                            <i className="far fa-2x fa-user-circle dropbtn"></i>
-                            <div className="dropdown">
-                                <Link className='nav-links' to={`/user/${this.props.currentUserId}`}>My Profile</Link>
-                                <p onClick={() => this.props.logout()} className='nav-links' >Logout</p>
-                            </div>
-                        </div>
+                    <div className="dropdown-container">
+                        <i className="far fa-2x fa-user-circle dropbtn"></i>
+                        {dropdown}
+                    </div>
 
-                        <div id='search-bar-container' >
-                            <input id="search-bar" placeholder="Find a beer..." type="text" />
-                            <i className="fas fa-search fa-med search-icon"></i>
-                        </div>
+
+                    <div id='search-bar-container' >
+                        <input id="search-bar" placeholder="Find a beer..." type="text" />
+                        <i className="fas fa-search fa-med search-icon"></i>
                     </div>
                 </div>
-
             </div>
 
-        )
-    }
+        </div>
+
+    )
+
 }
 
 export default NavBar
