c5777f3195e76aecfed18283d0b17eab5ea0b3fe
Domenico Daniel Giovinazzo <domenicodanielgiovinazzo@gmail.com>
2021-03-09 01:01:49 -0500

    time util

--- frontend/util/time_util.js
+++ frontend/util/time_util.js
@@ -1,7 +1,7 @@
 export const elapsedTime = (dateString)=>{
     const currentDate = new Date()
     const targetTime = new Date(dateString).getTime()
-    // debugger
+    debugger
     const elapsedTime = (currentDate.getTime() - targetTime) / 1000
     const currentMonth = (currentDate.getMonth()+1)
     const currentYear = (currentDate.getFullYear())
@@ -14,8 +14,8 @@ export const elapsedTime = (dateString)=>{
     else if(elapsedTime < 7200) return `an hour ago`
     else if(elapsedTime < 86400) return `${Math.floor(elapsedTime / 3600)} hours ago`
 
-    else if(elapsedTime < 172,800) return `yesterday`
-    else if(elapsedTime < 2678400 && [1,3,5,7,8,10,12].includes(currentMonth)) return `${Math.floor(elapsedTime / 86400)} days ago`
+    else if(elapsedTime >=86400 && elapsedTime < 172,800) return `yesterday`
+    else if(elapsedTime >= 172,800 && elapsedTime < 2678400 && [1,3,5,7,8,10,12].includes(currentMonth)) return `${Math.floor(elapsedTime / 86400)} days ago`
     else if(elapsedTime < 2592000 && [11,4,6,9].includes(currentMonth)) return `${Math.floor(elapsedTime / 86,400)} days ago`
     else if(elapsedTime < 2505600 && currentYear % 4 === 0) return `${Math.floor(elapsedTime / 86400)} days ago`
     else if(elapsedTime < 2419200 && currentYear % 4 !== 0) return `${Math.floor(elapsedTime / 86400)} days ago`
